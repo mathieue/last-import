2783eb5fc716bac00ad21b301ede2c461f7f9c12
Mathieu Elie <mathieuelie@gmail.com>
2012-08-28 23:35:22 +0200

    initial commit

---
+++ grab.rb
@@ -0,0 +1,42 @@
+require 'open-uri'
+require 'json'
+
+if ARGV.size != 2
+  puts "usage: ruby grab.rb api_key user"
+  exit 0
+end
+
+API_KEY = ARGV.shift
+user = ARGV.shift
+
+puts "fetching #{user} with api key #{API_KEY}"
+
+def get(user, page = 1)
+  url = "http://ws.audioscrobbler.com/2.0/?format=json&method=user.getrecenttracks&limit=100"
+  url += "&user=#{user}"
+  url += "&api_key=#{API_KEY}"
+  url += "&page=#{page.to_s}"
+  json_object = JSON.parse(open(url).read)
+end
+
+f = File.open('data.json', 'w')
+f.write '['
+
+index = 1
+total_page = 1
+
+while index <= total_page
+  puts "grabbing #{index}..."
+
+  json_object = get(user, index)
+
+  tracks = json_object['recenttracks']['track']
+  total_page = json_object['recenttracks']['@attr']['totalPages'].to_i
+  tracks.each do |t|
+    f.write t
+    f.write ", \n"
+  end
+  index += 1
+end
+
+f.write ']'
